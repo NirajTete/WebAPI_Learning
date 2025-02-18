728e1625e35845ca89ed2fb09d6e6922c33c6d5b
Dhiraj <Dhiraj@DESKTOP-VFU321B>
2025-02-18 11:12:30 +0530

    Auto Mapper config

--- CollegeApp/Config/AutoMapperConfig.cs
+++ CollegeApp/Config/AutoMapperConfig.cs
@@ -11,7 +11,16 @@ namespace WebAPI_Learning.Config
             //CreateMap<StudentDTO, Student>(); // To map StudentDTO -> Student
             //CreateMap<Student, StudentDTO>(); // To map Student -> StudentDTO
 
-            CreateMap<StudentDTO , Student>().ReverseMap(); // It will map both StudentDTO <-> Student
+            // To Map diffrent prop name  NOTE: before ReverseMap() it Map StudentDTO -> Student and after it Map StudentDTO <- Student
+            //CreateMap<StudentDTO , Student>().ReverseMap().ForMember(n => n.Name, opt => opt.MapFrom(x => x.StudentName)); // It will map both StudentDTO <-> Student
+
+            // To Ignore Specific prop   NOTE: before ReverseMap() it Map StudentDTO -> Student and after it Map StudentDTO <- Student
+            //CreateMap<StudentDTO , Student>().ReverseMap().ForMember(n => n.StudentName, opt => opt.Ignore()); // It will map both StudentDTO <-> Student
+
+            // To transform the null value 
+            //CreateMap<StudentDTO , Student>().ReverseMap().AddTransform<string>(n => string.IsNullOrEmpty(n) ? "No address found" : n); // It will map both StudentDTO <-> Student
+
+            CreateMap<StudentDTO, Student>().ReverseMap();
         }
     }
 }
