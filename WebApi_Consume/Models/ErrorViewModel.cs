d035389c1de8912a5579b56b161462806b8f01c7
Dhiraj <Dhiraj@DESKTOP-VFU321B>
2025-02-22 15:32:18 +0530

    New project added to consume the api

---
+++ WebApi_Consume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApi_Consume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApi_Consume/Repository/Services/ApiService.cs
@@ -0,0 +1,114 @@
+﻿using WebApi_Consume.Repository.Implementation;
+
+namespace WebApi_Consume.Repository.Services
+{
+    public class ApiService : IApiService
+    {
+        private readonly HttpClient _httpClient;
+        public ApiService(HttpClient httpClient )
+        {
+            _httpClient = httpClient;
+            _httpClient.Timeout = TimeSpan.FromSeconds(300); // Increase to 200 seconds
+        }
+
+        private HttpRequestMessage CreateRequest(HttpMethod method, string url, string token, HttpContent? content = null)
+        {
+            var request = new HttpRequestMessage(method, url)
+            {
+                Content = content
+            };
+
+            request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            return request;
+        }
+
+        public async Task<T> GetAsync<T>(string url, string token)
+        {
+            try
+            {
+                var request = CreateRequest(HttpMethod.Get, url, token);
+                var response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<T>();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<T> PostAsync<T>(string url, object data, string token)
+        {
+            try
+            {
+                var request = CreateRequest(HttpMethod.Post, url, token, JsonContent.Create(data));
+                var response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<T>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<T> PutAsync<T>(string url, object data, string token)
+        {
+            try
+            {
+                var request = CreateRequest(HttpMethod.Put, url, token, JsonContent.Create(data));
+                var response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<T>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<T> PatchAsync<T>(string url, object data, string token)
+        {
+            try
+            {
+                var request = CreateRequest(HttpMethod.Patch, url, token, JsonContent.Create(data));
+                var response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadFromJsonAsync<T>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public async Task<T> DeleteAsync<T>(string url, string token)
+        {
+
+            try
+            {
+                var request = CreateRequest(HttpMethod.Delete, url, token);
+                var response = await _httpClient.SendAsync(request);
+
+                // Ensure the response is successful before trying to deserialize
+                response.EnsureSuccessStatusCode();
+
+                // Deserialize and return the response content as type T
+                return await response.Content.ReadFromJsonAsync<T>();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+    }
+}
