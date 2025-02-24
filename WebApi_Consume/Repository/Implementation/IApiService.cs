fb3628ccc8a49f663b2cabea39e4e3667345e1da
Dhiraj <Dhiraj@DESKTOP-VFU321B>
2025-02-24 10:51:43 +0530

    Final project

---
+++ WebApi_Consume/Repository/Implementation/IApiService.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi_Consume.Repository.Implementation
+{
+    public interface IApiService
+    {
+        //Task<T> GetAsync<T>(string url, string token);
+        Task<T> GetAsync<T>(string url);
+        Task<T> PostAsync<T>(string url, object data, string token);
+        Task<T> PutAsync<T>(string url, object data, string token);
+        Task<T> PatchAsync<T>(string url, object data, string token);
+        Task<T> DeleteAsync<T>(string url, string token); // Update here
+    }
+}
